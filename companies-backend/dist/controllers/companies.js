85de39f4fbacb7f4407cd08a6456fbd1d435e6bd
turisa <tesa.jurisic1@gmail.com>
2021-10-31 10:07:42 +0100

    Initialize project

---
+++ companies-backend/dist/controllers/companies.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=companies.js.map
---
+++ companies-backend/src/index.ts
@@ -0,0 +1,8 @@
+const faker = require('faker');
+console.log(faker.name.jobTitle());
+
+const jobTitle = faker.name.jobTitle();
+const validUntil = new Date();
+
+const jobDescription = `We're looking for ${jobTitle.toLowerCase()}s to help us with our new project.`;
+console.log(jobDescription);
