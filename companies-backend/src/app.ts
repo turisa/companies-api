08b7d882ace0dd9fe63a78f7758f93092572c0f0
turisa <tesa.jurisic1@gmail.com>
2021-10-31 22:31:35 +0100

    Add routers

--- companies-backend/src/app.ts
+++ companies-backend/src/app.ts
@@ -1,5 +1,15 @@
 import express from 'express';
 
+import companiesRouter from './controllers/companies';
+import countriesRouter from './controllers/countries';
+import jobsRouter from './controllers/jobs';
+import managersRouter from './controllers/managers';
+
 const app = express();
+
+app.use('/api/companies', companiesRouter);
+app.use('/api/countries', countriesRouter);
+app.use('/api/jobs', jobsRouter);
+app.use('/api/managers', managersRouter);
 
 export default app;
