ce5612eaf3fe8a10b097e78423ab960b44fc091d
turisa <tesa.jurisic1@gmail.com>
2021-10-31 18:34:23 +0100

    Refactor

--- companies-backend/src/models/company.ts
+++ companies-backend/src/models/company.ts
@@ -20,6 +20,7 @@ const companySchema = new mongoose.Schema({
 companySchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
+
     delete returnedObject._id;
     delete returnedObject.__v;
   },
