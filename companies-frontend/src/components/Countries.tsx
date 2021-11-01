54816e2485cac96779548e191a027ddddec94f25
turisa <tesa.jurisic1@gmail.com>
2021-11-01 16:49:42 +0100

    Fix useEffect method

--- companies-frontend/src/components/Countries.tsx
+++ companies-frontend/src/components/Countries.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
+
+import Country from '../types/Country';
+import countriesService from '../services/countriesService';
+
 const Countries = () => {
-  return <h2>countries</h2>;
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  useEffect(() => {
+    countriesService.getAll().then((result) => setCountries(result));
+  }, []);
+
+  return (
+    <div>
+      <h2>Countries</h2>
+      {countries.map((country) => (
+        <p>{country.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Countries;
