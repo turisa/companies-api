66b1aeac2f0aed05d0a60a09a15d39d362214177
turisa <tesa.jurisic1@gmail.com>
2021-11-01 16:49:55 +0100

    Fix useEffect method

--- companies-frontend/src/components/Companies.tsx
+++ companies-frontend/src/components/Companies.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
+
+import Company from '../types/Company';
+
+import companiesService from '../services/companiesService';
+
 const Companies = () => {
-  return <h2>companies</h2>;
+  const [companies, setCompanies] = useState<Company[]>([]);
+  useEffect(() => {
+    companiesService.getAll().then((companies) => setCompanies(companies));
+  }, []);
+
+  return (
+    <div>
+      <h2>Companies</h2>
+      {companies.map((company) => (
+        <p>{company.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Companies;
