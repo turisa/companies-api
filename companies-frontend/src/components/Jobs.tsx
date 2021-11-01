7e0a0d4e42743f09fe449150471ff8feb75514ff
turisa <tesa.jurisic1@gmail.com>
2021-11-01 16:49:26 +0100

    Fix useEffect method

--- companies-frontend/src/components/Jobs.tsx
+++ companies-frontend/src/components/Jobs.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+import jobsService from '../services/jobsService';
+import Job from '../types/Job';
+
 const Jobs = () => {
-  return <h1>jobs</h1>;
+  const [jobs, setJobs] = useState<Job[]>([]);
+
+  useEffect(() => {
+    jobsService.getAll().then((result) => setJobs(result));
+  }, []);
+
+  return (
+    <div>
+      <h2>Jobs</h2>
+      {jobs.map((job) => (
+        <p>{job.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Jobs;
