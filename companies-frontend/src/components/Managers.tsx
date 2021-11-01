070ab4b8726f5252ba65cdf3320916181886954d
turisa <tesa.jurisic1@gmail.com>
2021-11-01 16:51:33 +0100

    Add service

--- companies-frontend/src/components/Managers.tsx
+++ companies-frontend/src/components/Managers.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+import managersService from '../services/managersService';
+import Manager from '../types/Manager';
+
 const Managers = () => {
-  return <h1>managers</h1>;
+  const [managers, setManagers] = useState<Manager[]>([]);
+
+  useEffect(() => {
+    managersService.getAll().then((result) => setManagers(result));
+  }, []);
+
+  return (
+    <div>
+      <h2>Managers</h2>
+      {managers.map((manager) => (
+        <p>{manager.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Managers;
