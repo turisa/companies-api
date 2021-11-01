df60898b38fb7ce2be3acab7f1c1ea645b34f622
turisa <tesa.jurisic1@gmail.com>
2021-11-01 21:48:34 +0100

    Refactor

--- companies-frontend/src/App.tsx
+++ companies-frontend/src/App.tsx
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route, Redirect, Link } from 'react-router-dom';
+
 import Companies from './components/Companies';
 import Countries from './components/Countries';
 import Jobs from './components/Jobs';
 import Managers from './components/Managers';
 import Navbar from './components/Navbar';
 
+import Company from './types/Company';
+import Country from './types/Country';
+import Job from './types/Job';
+import Manager from './types/Manager';
+
+import companiesService from './services/companiesService';
+import countriesService from './services/countriesService';
+import jobsService from './services/jobsService';
+import managersService from './services/managersService';
+
 function App() {
+  const [companies, setCompanies] = useState<Company[]>([]);
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [managers, setManagers] = useState<Manager[]>([]);
+
+  useEffect(() => {
+    companiesService.getAll().then((companies) => setCompanies(companies));
+  }, []);
+
+  useEffect(() => {
+    countriesService.getAll().then((result) => setCountries(result));
+  }, []);
+
+  useEffect(() => {
+    jobsService.getAll().then((result) => setJobs(result));
+  }, []);
+
+  useEffect(() => {
+    managersService.getAll().then((result) => setManagers(result));
+  }, []);
+
   return (
-    <div className="App">
+    <div className="bg-gray-100">
       <Navbar />
       <Switch>
         <Route path="/companies">
-          <Companies />
+          <Companies companies={companies} />
         </Route>
         <Route path="/countries">
-          <Countries />
+          <Countries countries={countries} />
         </Route>
         <Route path="/jobs">
-          <Jobs />
+          <Jobs jobs={jobs} />
         </Route>
         <Route path="/managers">
-          <Managers />
+          <Managers managers={managers} />
         </Route>
         <Route path="/">
           <Redirect to="/companies" />
