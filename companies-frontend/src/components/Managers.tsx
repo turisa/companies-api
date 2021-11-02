0443a1bb2db47e59e97870d7f86d306d532ee1a3
turisa <tesa.jurisic1@gmail.com>
2021-11-02 19:48:05 +0100

    Add search bar

--- companies-frontend/src/components/Managers.tsx
+++ companies-frontend/src/components/Managers.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Manager from '../types/Manager';
 import managersService from '../services/managersService';
 import { useHistory } from 'react-router';
+import SearchBar from './SearchBar';
 
 const Managers = () => {
   const [managers, setManagers] = useState<Manager[]>([]);
@@ -16,12 +17,19 @@ const Managers = () => {
     history.push(`managers/${id}`);
   };
 
+  const searchJobs = (searchInput: string) => {
+    managersService.getAll({ name: searchInput }).then((response) => {
+      setManagers(response);
+    });
+  };
+
   return (
     <div className="flex flex-col items-center gap-y-2 pt-24">
+      <SearchBar onSubmit={searchJobs} />
       {managers.map((manager) => (
         <div
           onClick={() => viewDetails(manager.id)}
-          className="grid grid-cols-2 w-8/12 xl:w-1/3 p-3 bg-white shadow-sm max-h-32 transition ease-linear duration-100 hover:shadow-md"
+          className="grid grid-cols-2 w-full xl:w-8/12 p-3 bg-white shadow-sm max-h-32 transition ease-linear duration-100 hover:shadow-md"
           key={manager.id}
         >
           <h2 className="text-gray-500">{manager.name}</h2>
