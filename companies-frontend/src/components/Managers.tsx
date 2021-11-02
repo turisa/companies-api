935b22132712d5410ad11af41560f078c368a737
turisa <tesa.jurisic1@gmail.com>
2021-11-02 12:40:52 +0100

    Enable detail view

--- companies-frontend/src/components/Managers.tsx
+++ companies-frontend/src/components/Managers.tsx
@@ -1,10 +1,26 @@
-import Manager from '../types/Manager';
+import { useState, useEffect } from 'react';
 
-const Managers = ({ managers }: { managers: Manager[] }) => {
+import Manager from '../types/Manager';
+import managersService from '../services/managersService';
+import { useHistory } from 'react-router';
+
+const Managers = () => {
+  const [managers, setManagers] = useState<Manager[]>([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    managersService.getAll().then((result) => setManagers(result));
+  }, []);
+
+  const viewDetails = (id: string) => {
+    history.push(`managers/${id}`);
+  };
+
   return (
     <div className="flex flex-col items-center gap-y-2 pt-24">
       {managers.map((manager) => (
         <div
+          onClick={() => viewDetails(manager.id)}
           className="grid grid-cols-2 w-8/12 xl:w-1/3 p-3 bg-white shadow-sm max-h-32 transition ease-linear duration-100 hover:shadow-md"
           key={manager.id}
         >
