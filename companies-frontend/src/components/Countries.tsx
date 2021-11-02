71151e6868c1d6cd483311190f36c4893c698e4d
turisa <tesa.jurisic1@gmail.com>
2021-11-02 19:38:12 +0100

    Add search bar

--- companies-frontend/src/components/Countries.tsx
+++ companies-frontend/src/components/Countries.tsx
@@ -3,10 +3,17 @@ import { useState, useEffect } from 'react';
 import Country from '../types/Country';
 import countriesService from '../services/countriesService';
 import { useHistory } from 'react-router';
+import SearchBar from './SearchBar';
 
 const Countries = () => {
   const [countries, setCountries] = useState<Country[]>([]);
   const history = useHistory();
+
+  const searchCountries = (searchInput: string) => {
+    countriesService.getAll({ name: searchInput }).then((response) => {
+      setCountries(response);
+    });
+  };
 
   useEffect(() => {
     countriesService.getAll().then((result) => setCountries(result));
@@ -18,10 +25,11 @@ const Countries = () => {
 
   return (
     <div className="flex flex-col items-center gap-y-2 pt-24">
+      <SearchBar onSubmit={searchCountries} />
       {countries.map((country) => (
         <div
           onClick={() => viewDetails(country.id)}
-          className="grid grid-cols-2 w-8/12 xl:w-3/12 p-3 bg-white shadow-sm max-h-32 transition ease-linear duration-100 hover:shadow-md"
+          className="grid grid-cols-2  w-full xl:w-8/12 p-3 bg-white shadow-sm max-h-32 transition ease-linear duration-100 hover:shadow-md"
           key={country.id}
         >
           <h2 className="text-gray-500">{country.name}</h2>
