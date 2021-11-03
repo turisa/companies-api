bbbb1579812b5fc6736c647d1a0d84e42ab522ce
turisa <tesa.jurisic1@gmail.com>
2021-11-03 17:48:02 +0100

    Remove unused fields

--- companies-backend/src/models/user.ts
+++ companies-backend/src/models/user.ts
@@ -4,12 +4,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
   name: String,
   passwordHash: String,
-  blogs: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Blog',
-    },
-  ],
 });
 
 userSchema.set('toJSON', {
