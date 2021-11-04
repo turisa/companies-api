53a88a86c86ee4a3f6c9aff4f7bad6f31f7b7ace
turisa <tesa.jurisic1@gmail.com>
2021-11-04 11:44:38 +0100

    Improve error handling

--- companies-backend/src/controllers/login.ts
+++ companies-backend/src/controllers/login.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import express from 'express';
+import express, { response } from 'express';
 import jsonwebtoken from 'jsonwebtoken';
 
 import User from '../models/user';
@@ -9,13 +9,17 @@ const loginRouter = express.Router();
 loginRouter.post('/', async (request, response) => {
   const body = request.body;
 
+  if (!body || !body.username || !body.password) {
+    return response.status(400).json({ error: 'Username or password missing' });
+  }
+
   const user = await User.findOne({ username: body.username });
   const passwordCorrect = user
     ? await bcrypt.compare(body.password, user.passwordHash)
     : false;
 
   if (!user || !passwordCorrect) {
-    return response.status(401).json({ error: 'invalid username or password' });
+    return response.status(401).json({ error: 'Invalid username or password' });
   }
 
   const userForToken = {
