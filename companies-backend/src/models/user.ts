d71cbab8e874e17c2a50f5b46283b8065e02c4f6
turisa <tesa.jurisic1@gmail.com>
2021-11-05 13:54:00 +0100

    Remove unused imports

--- companies-backend/src/models/user.ts
+++ companies-backend/src/models/user.ts
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 import mongooseUniqueValidator from 'mongoose-unique-validator';
 import { ICompany } from './company';
 
