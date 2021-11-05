b841f12b4fcd7e130d5ffacc82c4c2122f279a5f
turisa <tesa.jurisic1@gmail.com>
2021-11-05 16:36:27 +0100

    Add model for review

---
+++ companies-backend/src/models/review.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+import { ICompany } from './company';
+import { IUser } from './user';
+
+export interface IReview extends mongoose.Document {
+  content: string;
+
+  company: ICompany;
+  user: IUser;
+}
+
+const reviewSchema = new mongoose.Schema({
+  content: String,
+
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Company',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+reviewSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Review: mongoose.Model<IReview> = mongoose.model('Review', reviewSchema);
+
+export default Review;
