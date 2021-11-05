f502b206d888a3d7ca2459cea288e1cdcd88ccbe
turisa <tesa.jurisic1@gmail.com>
2021-11-05 22:45:47 +0100

    Include error handler

--- companies-backend/src/app.ts
+++ companies-backend/src/app.ts
@@ -2,13 +2,16 @@ import cors from 'cors';
 import express from 'express';
 
 import loginRouter from './controllers/login';
+import reviewsRouter from './controllers/reviews';
 import usersRouter from './controllers/users';
 import companiesRouter from './controllers/companies';
 import countriesRouter from './controllers/countries';
 import jobsRouter from './controllers/jobs';
 import managersRouter from './controllers/managers';
 
-import { requestLogger, tokenVerifier } from './utils/middleware';
+import { requestLogger, tokenVerifier, errorHandler } from './utils/middleware';
+
+require('express-async-errors');
 
 const app = express();
 
@@ -19,9 +22,12 @@ app.use(requestLogger);
 
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/reviews', tokenVerifier, reviewsRouter);
 app.use('/api/companies', tokenVerifier, companiesRouter);
 app.use('/api/countries', tokenVerifier, countriesRouter);
 app.use('/api/jobs', tokenVerifier, jobsRouter);
 app.use('/api/managers', tokenVerifier, managersRouter);
+
+app.use(errorHandler);
 
 export default app;
