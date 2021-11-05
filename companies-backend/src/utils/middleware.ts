9e8ff3172d09146e62eef9ed6aa829f5ea8aa63d
turisa <tesa.jurisic1@gmail.com>
2021-11-05 13:38:26 +0100

    Add token verifier

--- companies-backend/src/utils/middleware.ts
+++ companies-backend/src/utils/middleware.ts
@@ -1,20 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
 import http from 'http';
+import jsonwebtoken from 'jsonwebtoken';
 
 interface IncomingMessageWithBody extends http.IncomingMessage {
   body: string;
 }
 
-export const requestLogger = () => {
-  morgan.token('body', (request: IncomingMessageWithBody) => {
-    return JSON.stringify(request.body);
-  });
+morgan.token('body', (request: IncomingMessageWithBody) => {
+  return JSON.stringify(request.body);
+});
 
-  return morgan(
-    ':method :url :status :res[content-length] - :response-time ms :body'
-  );
-};
+export const requestLogger = morgan(
+  ':method :url :status :res[content-length] - :response-time ms :body'
+);
 
 export const tokenExtractor = (
   request: express.Request,
@@ -27,6 +26,26 @@ export const tokenExtractor = (
     authorization && authorization.toLowerCase().startsWith('bearer ')
       ? authorization.substring(7)
       : null;
+
+  next();
+};
+
+export const tokenValidator = (
+  request: express.Request,
+  response: express.Response,
+  next: express.NextFunction
+) => {
+  const token = request.token;
+
+  const decodedToken = token
+    ? jsonwebtoken.verify(token, process.env.SECRET)
+    : null;
+
+  if (!token || !decodedToken) {
+    return response.status(401).json({
+      error: 'Token missing or invalid',
+    });
+  }
 
   next();
 };
