32f0d44d753ad71b7f945e75d80e25fe27b0d0a1
turisa <tesa.jurisic1@gmail.com>
2021-11-05 13:38:39 +0100

    Use token validators

--- companies-backend/src/app.ts
+++ companies-backend/src/app.ts
@@ -8,16 +8,24 @@ import countriesRouter from './controllers/countries';
 import jobsRouter from './controllers/jobs';
 import managersRouter from './controllers/managers';
 
+import {
+  requestLogger,
+  tokenExtractor,
+  tokenValidator,
+} from './utils/middleware';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/companies', companiesRouter);
-app.use('/api/countries', countriesRouter);
-app.use('/api/jobs', jobsRouter);
-app.use('/api/managers', managersRouter);
+app.use('/api/companies', tokenExtractor, tokenValidator, companiesRouter);
+app.use('/api/countries', tokenExtractor, tokenValidator, countriesRouter);
+app.use('/api/jobs', tokenExtractor, tokenValidator, jobsRouter);
+app.use('/api/managers', tokenExtractor, tokenValidator, managersRouter);
 
 export default app;
