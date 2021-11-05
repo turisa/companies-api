c2b79c2019a7221f6642068cfd49de3ffbe960ed
turisa <tesa.jurisic1@gmail.com>
2021-11-05 22:42:31 +0100

    Add reviews controller

---
+++ companies-backend/src/controllers/reviews.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+import User from '../models/user';
+import Company from '../models/company';
+import Review from '../models/review';
+
+const reviewsRouter = express.Router();
+
+reviewsRouter.post('/', async (request, response) => {
+  const { content, companyId } = request.body;
+  const userId = request.token.id;
+
+  const user = await User.findById(userId);
+  const company = await Company.findById(companyId);
+
+  if (!company) {
+    return response.status(404).end();
+  }
+
+  const reviewObject = {
+    content,
+  };
+
+  const review = new Review(reviewObject);
+
+  review.company = company;
+  review.user = user;
+
+  const result = await review.save();
+
+  company.reviews = company.reviews.concat(result);
+  await company.save();
+
+  return response.status(201).json(result);
+});
+
+reviewsRouter.put('/:id', async (request, response) => {
+  const reviewId = request.params.id;
+  const userId = request.token.id;
+
+  const review = await Review.findById(reviewId);
+
+  if (review.deleted) {
+    return response.status(404).end();
+  }
+
+  if (review.user.id.toString('hex') !== userId) {
+    return response.status(401).json({
+      error: 'User is not the owner',
+    });
+  }
+
+  const newContent: string = request.body.content;
+  review.content = newContent;
+
+  const result = await review.save();
+
+  response.status(200).json(result);
+});
+
+reviewsRouter.delete('/:id', async (request, response) => {
+  const reviewId = request.params.id;
+  const userId = request.token.id;
+
+  const review = await Review.findById(reviewId);
+
+  if (review)
+    if (review.user.id.toString('hex') !== userId) {
+      return response.status(401).json({
+        error: 'User is not the owner',
+      });
+    }
+
+  review.deleted = true;
+  await review.save();
+
+  return response.status(204).end();
+});
+
+export default reviewsRouter;
